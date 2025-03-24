91e905b0c10b60e15fd6dbe4775a8cb609952a6d
Yigit Sirin <122676073+neraiv@users.noreply.github.com>
2025-03-24 15:01:28 +0300

    fixed scrool reveal

--- script.js
+++ script.js
@@ -1,4 +1,63 @@
+/*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+/*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    /*==================== sticky navbar ====================*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+/*==================== scroll reveal ====================*/
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+})
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/*==================== typed js ====================*/
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+    this.classList.toggle('bx-toggle-right');
+
+});
+
+
 document.addEventListener('DOMContentLoaded', function() {
+    
     const cards = document.querySelectorAll('.card');
     const filtersContainer = document.getElementById('filters');
     const searchInput = document.querySelector('.search-input');
@@ -84,4 +143,12 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         filterCards()
     });
+
+    const typed = new Typed('.typed-text', {
+        strings: [ 'Coder','Tech Enthusiast'],
+        typeSpeed: 200,
+        backDelay: 200,
+        loop: true
+    });
 });
+
